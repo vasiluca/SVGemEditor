8903e415a1f7efbfdce73fafc91591c722701809
Arshana Acharya <arshanaacharya@gmail.com>
2023-12-03 13:05:05 -0500

    "created export file to save/download"

---
+++ App/Tab/Export.js
@@ -0,0 +1,13 @@
+function download(data, filename, type) {
+    var file = new Blob([data], {type: type});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+    }
+}
