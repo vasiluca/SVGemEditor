1e0932956884264681e26c9b97ff4f4eea3b67b0
Snehitha03 <snehitha.nukala@gmail.com>
2023-12-03 18:46:03 -0500

    Added Selection and Resize test

--- App/Tests/Integration/SelectionResize.js
+++ App/Tests/Integration/SelectionResize.js
@@ -1,1 +1,20 @@
 //** Test Selection and Resize function (pass in to functions through parameters) */
+import { resize } from "../../CanvasElements/Modify/Transform/Resize.js";
+import { cache, pressed } from "../Cache.js";
+
+import { select } from "./Selection.js";
+
+function runtest(){
+    
+    test("Selection and Resize Test", () => {
+        var area = select.area(cache.ele);
+        resize(cache.ele);
+        pressed.shiftkey = true;
+        resize(cache.ele);
+        var new_area = select.area(cache.ele);
+        if (area == new_area){
+            throw new Error("The selected item is not resized properly");
+        }
+
+    });
+}
