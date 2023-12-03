0a8cdf0e942a8fcc4da7f33a7d9008ccf8684076
Luca Vasilache <lucavasilach@gmail.com>
2023-12-03 13:50:18 -0500

    Fixed Minor Issue

--- App/Tab/Property/OpacityEvents.js
+++ App/Tab/Property/OpacityEvents.js
@@ -1,6 +1,6 @@
-import { Opacityproperty } from "./Opacity";
+import { Opacityproperty } from "./Opacity.js";
 
-$().mousedown(function(e) {
-	// e.clientX //= users x Mouse Position
-	// e.clientY //= users y Mouse Position
-})
+// $().mousedown(function(e) {
+// 	// e.clientX //= users x Mouse Position
+// 	// e.clientY //= users y Mouse Position
+// })
