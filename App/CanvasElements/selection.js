d65481d58cf31317c5af69350d71dc6fcf6fc539
Luca Vasilache <lucavasilach@gmail.com>
2023-12-03 14:17:35 -0500

    Removed Unused Code from Area

--- App/CanvasElements/selection.js
+++ App/CanvasElements/selection.js
@@ -10,33 +10,11 @@ var select = {
 			height: cache.stop[1] > cache.start[1] ? cache.stop[1] - cache.start[1] : cache.start[1] - cache.stop[1]
 		}
 		if (ele != undefined) {
-			if (ele == true) {
-				if (!pressed.shiftKey) {
-					$('.outline').remove();
-					$('.outline2').remove();
-				}
-				cache.hoverEle.clone().removeAttr('id').addClass('outline').insertBefore(cache.hoverEle);
-				cache.hoverEle.clone().removeAttr('id').addClass('outline2').insertBefore($('.outline'));
-				$('.outline').attr({
-					'stroke': 'rgba(255,255,255,0.75)',
-					'stroke-width': parseInt(cache.ele.attr('stroke-width')) + 3.5,
-					'fill': 'transparent'
-				});
-				$('.outline2').attr({
-					'stroke': 'cornflowerblue',
-					'stroke-width': parseInt(cache.ele.attr('stroke-width')) + 7,
-					'fill': 'transparent'
-				});
-			} else if (ele == false) {
-				$('.outline').remove();
-				$('.outline2').remove();
-			} else {
-				area = {
-					x: ele[0].getBoundingClientRect().x,
-					y: ele[0].getBoundingClientRect().y,
-					width: ele[0].getBoundingClientRect().width,
-					height: ele[0].getBoundingClientRect().height
-				}
+			area = {
+				x: ele[0].getBoundingClientRect().x,
+				y: ele[0].getBoundingClientRect().y,
+				width: ele[0].getBoundingClientRect().width,
+				height: ele[0].getBoundingClientRect().height
 			}
 		}
 		this.ui(area);
