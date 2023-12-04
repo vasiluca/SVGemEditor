d61e49df9cefac75f6afb543b512c05d07170487
sheneeza <126906225+sheneeza@users.noreply.github.com>
2023-12-04 19:14:19 +0000

    edit selection and move integration test

--- App/Tests/Integration/SelectionMove.js
+++ App/Tests/Integration/SelectionMove.js
@@ -1,9 +1,22 @@
 //** Test Selection and Move function (pass in to functions through parameters) */
 import { cache, pressed } from "../Cache.js";
 
+import { move } from "../../CanvasElements/Modify/Transform/Move.js";
+
 import { select } from "./Selection.js";
 
 function runtest(){
-    
-    move(cache.ele);
+    test("Selection and Move Test", () => {
+        /* Set initial position */
+        var initialPosition = select.position(cache.ele);
+        move(cache.ele);
+        pressed.shiftkey = true;
+        move(cache.ele);
+        var newPosition = select.position(cache.ele);
+
+        // Check if the position has changed after the move operation
+        if (initialPosition.x === newPosition.x && initialPosition.y === newPosition.y) {
+            throw new Error("The selected item is not moved properly");
+
+    }
 }
