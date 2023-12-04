599d0543558787a4290da6ff27bd2365a5bca391
Arshana Acharya <arshanaacharya@gmail.com>
2023-12-04 18:50:38 -0500

    finished tests and slightly alterd export

--- App/Tab/Action/Export.js
+++ App/Tab/Action/Export.js
@@ -1,7 +1,9 @@
 function Export(data, filename, type) {
     var file = new Blob([data], {type: type});
-    if (window.navigator.msSaveOrOpenBlob) // IE10+
+    if (window.navigator.msSaveOrOpenBlob){ // IE10+
         window.navigator.msSaveOrOpenBlob(file, filename);
+        return true;
+    }
     else { // Others
         var a = document.createElement("a"),
                 url = URL.createObjectURL(file);
@@ -9,6 +11,9 @@ function Export(data, filename, type) {
         a.download = filename;
         document.body.appendChild(a);
         a.click();
+        return true;
     }
+
+    return false;
 }
 export {Export}
--- App/Tests/Unit/Export.test.js
+++ App/Tests/Unit/Export.test.js
@@ -1,3 +1,19 @@
 import { cache } from "../../Cache.js";
+import { Export } from "../../Tab/Action/Export.js"
 
+function exportTests(){
+    test("Document was not created\n", () => {
 
+        var content = $('#editor')[0].outerHTML;
+        filename = "testerfile";
+        expect(Export(content, filename, SVGUnitTypes).tobe('false'))
+    });
+
+    test("Document was created\n", () => {
+
+        var content = $('#editor')[0].outerHTML;
+        filename = "testerfile";
+        expect(Export(content, filename, SVGUnitTypes).tobe('true'))
+    });
+
+}
