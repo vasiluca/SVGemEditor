1046eb6ea8f6bf5458bc7337a2be1287547c3d46
Luca Vasilache <lucavasilach@gmail.com>
2023-12-03 19:42:47 -0500

    Export (Event) Functional

--- App/Tab/Action/ExportEvents.js
+++ App/Tab/Action/ExportEvents.js
@@ -2,9 +2,9 @@ import { Export } from "../Export.js";
 import {doc} from "../../SetUp.js";
 
 $('[aria-label="export"]').click(function() {
+	var content = $('#editor')[0].outerHTML; // outerHTML includes the parent tag selected as well as children elements
 	
-	console.log("export pressed");
-	fileName = prompt("What would you like to name your file?\n");
-	Export(doc, fileNmae, SVGUnitTypes);
-	// do your export logic here
+	var fileName = prompt("What would you like to name your SVG file?\n");
+	fileName += '.svg';
+	Export(content, fileName, SVGUnitTypes);
 })
