ad0501e3cdffaf775658c66eb96679241bb233e4
Luca Vasilache <lucavasilach@gmail.com>
2023-12-04 16:22:36 -0500

    Images Feature Partially Working

--- App/core.js
+++ App/core.js
@@ -26,6 +26,8 @@ import * as actions from './Tab/Action.js'; // No objects are actually exported 
 	import { Export } from './Tab/Action/Export.js';
 	import * as ExportEvents from './Tab/Action/ExportEvents.js';
 
+import * as ImageEvents from './Tab/Tool/ImageEvents.js';
+
 
 deleteButton.initialize();
 
@@ -41,28 +43,7 @@ $(document).ready(function () {
 		if (tool.name != 'selection') {
 			tool.type = 'selection';
 		}
-	}).mouseup(function (e) {
-		if (tool.name == 'image') {
-			if (tool.imageIndex != -1) {
-				var selection = tool.images[tool.imageIndex];
-				var posX = e.clientX - selection.width / 2;
-				var posY = e.clientY - selection.height / 2;
-				// was previously selection.height and selection.width for height and width
-				var element = '<image xlink:href="' + selection.result + '" height="' + 500 + '" width="auto" x="' + posX + '" y="' + posY + '"/>';
-				$('#editor').html($('#editor').html() + element);
-				if (!pressed.ctrlKey && tool.imageIndex > -1) {
-					tool.imageIndex--;
-				}
-			}
-			if (tool.imageIndex == -1) {
-				tool.type = 'selection';
-			}
-		}
-		if (pressed.spaceBar) {
-			ui.cursor('grab');
-		}
-		layers.update();
-	});
+	})
 
 
 
