47696e7c87eb869a65a2e0aed07cd3671c654f15
Luca Vasilache <lucavasilach@gmail.com>
2023-12-04 21:00:53 -0500

    Selection Resize Integration Test Improvements

--- App/Tests/Integration/SelectionResize.test.js
+++ App/Tests/Integration/SelectionResize.test.js
@@ -10,10 +10,18 @@ import { editSVG } from "../../CanvasElements/Modify/editSVG.js";
 function runtest(){
     
     test("Selection and Resize Test", () => {
+        newSVG.create("rectangle");
         var area = select.area(cache.ele);
+        
+        cache.start = [0,0];
+        cache.stop = [10,10];
         resize(cache.ele);
+
         pressed.shiftkey = true;
+        cache.start = [0, 0];
+        cache.stop = [20, 20];
         resize(cache.ele);
+
         var new_area = select.area(cache.ele);
         if (area == new_area){
             throw new Error("The selected item is not resized properly");
